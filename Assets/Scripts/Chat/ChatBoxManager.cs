73b5de361067f7cdbf49682e12dee8917624189f
CheeseMans <109389539+KennethF10293@users.noreply.github.com>
2022-07-27 09:42:07 -0400

    Added Chat

--- Assets/Scripts/Chat/ChatBoxManager.cs
+++ Assets/Scripts/Chat/ChatBoxManager.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
 
 
-public class ChatBoxManager : MonoBehaviour
+public class ChatBoxManager : MonoBehaviourPun
 {
     public TMP_InputField inputField;
     public TMP_Text placeholderText;
+    [HideInInspector]
+    public int textBoxCount = 0;
+    Color[] textBoxColors = new Color[] {new Color(0.8584906f, 0.8584906f, 0.8584906f, 1f), new Color(0.95f, 0.95f, 0.95f)};
+    public GameObject messagePrefab;
+    public GameObject messageContainer;
+    bool chatboxSelected = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +27,31 @@ public class ChatBoxManager : MonoBehaviour
     void Update()
     {
         bool playerTurn = PictManager.playerID == PictManager.currentDrawer;
-        inputField.interactable = (!playerTurn);
+        //inputField.interactable = (!playerTurn);
         placeholderText.text = (playerTurn) ? "Chat" : "Enter Guess";
+        if(chatboxSelected && Input.GetKeyDown(KeyCode.Return))
+        {
+            NetworkAddTextBox();
+        }
+    }
+
+    [PunRPC]
+    public void AddTextBox(string message, string sender)
+    {
+        textBoxCount++;
+        ChatMessage newMessage = Instantiate(messagePrefab).GetComponent<ChatMessage>();
+        newMessage.transform.parent = messageContainer.transform;
+        newMessage.background.color = textBoxColors[textBoxCount%2];
+        newMessage.message.text = "<color=grey>"+sender + "</color>: " + message;
+    }
+
+    public void NetworkAddTextBox()
+    {
+        photonView.RPC("AddTextBox", RpcTarget.AllBufferedViaServer, inputField.text, PhotonNetwork.NickName);
+    }
+
+    public void ChatBoxSelected(bool value)
+    {
+        chatboxSelected = value;
     }
 }
--- Assets/Scripts/Game Management/PictManager.cs
+++ Assets/Scripts/Game Management/PictManager.cs
@@ -8,12 +8,16 @@ public class PictManager : MonoBehaviourPun
 {
     public static int playerID = 0;
     public int playerCount = 1;
+    public int round = 1;
     public static int currentDrawer = 0;
     public static string currentWord;
 
     public GameObject[] drawerPanels;
     public GameObject[] guesserPanels;
-    bool switchPanels = true;
+    public TMPro.TMP_Text roundText;
+    bool switchPanels = false;
+    [HideInInspector]
+    public int guessedWord = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,15 +31,26 @@ public class PictManager : MonoBehaviourPun
         {
             photonView.RPC("SendID", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.UserId);
         }
+        switchPanels = (playerID != currentDrawer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerID == currentDrawer && switchPanels)
+        if(switchPanels != (playerID == currentDrawer))
         {
             //Switch between the guesser panels and drawer panels
+            switchPanels = !switchPanels;
+            foreach(GameObject guesserPanel in guesserPanels)
+            {
+                guesserPanel.SetActive(!switchPanels);
+            }
+            foreach(GameObject drawerPanel in drawerPanels)
+            {
+                drawerPanel.SetActive(switchPanels);
+            }      
         }
+        roundText.text = "Round " + round.ToString() + "/" + GameSettings.rounds.ToString();
     }
 
     [PunRPC]
